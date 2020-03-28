52670539c7cfd11334d005d5834624fb76ad0edf
tpk@tb1 <admin@reverie.tech>
2020-03-28 02:33:18 +0000

    added kubernetes and circleci config

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -6,10 +6,10 @@
 dockerpath=revtec/ml_ops
 
 # Run the Docker Hub container with kubernetes
-kubectl run prediction-app --image=$dockerpath --port=80
+kubectl run pred-app --image=$dockerpath --port=80
 
 # List kubernetes pods
 kubectl get pods
 
 # Forward the container port to a host
- kubectl port-forward deployment/prediction-app  8080:80
+kubectl port-forward deployment/pred-app  8000:80
